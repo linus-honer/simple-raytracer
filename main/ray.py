4261f15abf173eab567f990cca93b1193ec39345
Linus Honer <linushoner0@gmail.com>
2024-02-21 21:12:05 -0600

    More stuff for ray

--- main/ray.py
+++ main/ray.py
@@ -1,6 +1,8 @@
-from .utils.vector3 import vec3, extract, rgb
 import numpy as np
 from functools import reduce as reduce
+
+from .utils.constants import *
+from .utils.vector3 import vec3, extract, rgb
 
 class Ray:
     def __init__(self, origin, dir, depth, n, reflections, transmissions, diffuse_reflections):
@@ -38,3 +40,33 @@ class Hit:
         if self.N is None:
             self.N = self.collider.get_N(self)
         return self.N
+    
+def get_raycolor(ray, scene):
+    inters = [s.intersect(ray.origin, ray.dir) for s in scene.collider_list]
+    distances, hit_orientation = zip(*inters)
+
+    nearest = reduce(np.minimum, distances)
+    color = rgb(0., 0., 0.)
+    
+    for (coll, dis , orient) in zip(scene.collider_list, distances, hit_orientation):
+        hit_check = (nearest != FARAWAY) & (dis == nearest)
+
+        if np.any(hit_check):
+            material = coll.assigned_primitive.material
+            hit_info = Hit(extract(hit_check,dis) , extract(hit_check,orient), material, coll, coll.assigned_primitive)
+            
+            cc = material.get_color(scene,  ray.extract(hit_check), hit_info)
+            color += cc.place(hit_check)
+
+    return color
+
+def get_distances(ray, scene):
+    inters = [s.intersect(ray.origin, ray.dir) for s in scene.collider_list]
+    distances, hit_orientation = zip(*inters)
+
+    nearest = reduce(np.minimum, distances)
+    
+    max_r_distance = 10
+    r_distance = np.where(nearest <= max_r_distance, nearest, max_r_distance)
+    norm_r_distance = r_distance/max_r_distance
+    return rgb(norm_r_distance, norm_r_distance, norm_r_distance)
