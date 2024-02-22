1bb74925cae5102ffc659ba419576e9a0e542af1
Linus Honer <linushoner0@gmail.com>
2024-02-21 20:53:22 -0600

    Camera Thing

---
+++ main/camera.py
@@ -0,0 +1,28 @@
+from .utils.vector3 import vec3, rgb
+import numpy as np
+from .ray import Ray
+
+class Camera():
+    def __init__(self, look_from, look_at, screen_width = 400 ,screen_height = 300,  field_of_view = 90., aperture = 0., focal_distance = 1.):
+        self.screenWidth = screen_width
+        self.screenHeight = screen_height
+        self.aspectRatio = float(screen_width) / screen_height
+
+        self.lookFrom = look_from
+        self.lookAt = look_at
+        self.cameraWidth = np.tan(field_of_view * np.pi/180   /2.)*2.
+        self.cameraHeight = self.camera_width/self.aspect_ratio
+
+        self.cameraFwd = (look_at - look_from).normalize()
+        self.cameraRight = (self.cameraFwd.cross(vec3(0.,1.,0.))).normalize()
+        self.cameraUp = self.cameraRight.cross(self.cameraFwd)
+
+        self.lensRadius =  aperture / 2.
+        self.focalDistance = focal_distance
+
+        self.x = np.linspace(-self.camera_width/2., self.camera_width/2., self.screen_width)
+        self.y = np.linspace(self.camera_height/2., -self.camera_height/2., self.screen_height)
+
+        xx,yy = np.meshgrid(self.x,self.y)
+        self.x = xx.flatten()
+        self.y = yy.flatten()
